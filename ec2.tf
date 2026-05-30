2b33b9bff656241370adea90bf27d9315e161e38
lina <rathodlina0206@gmail.com>
2026-05-30 22:02:08 +0530

    Added EC2 Terraform

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "myec2" {
+  ami                    = "ami-091138d0f0d41ff90"
+  instance_type          = "t3.micro"
+  key_name               = "my-new-key"
+  vpc_security_group_ids = ["sg-0ae6f7e3e8be7e9d6"]
+  tags = {
+    Name = "myec2"
+    name = "lina"
+    env  = "dev"
+
+  }
+
+}
