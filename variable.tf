6ea0bd569f403889285a636c0b808ce2dc1daf9c
lina <rathodlina0206@gmail.com>
2026-06-03 23:07:52 +0530

    this is playbook

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+    default = "us-east-1"
+  }
+  variable "mybucket" {
+    default = ""
+  }
+  variable "ami" {
+    default = "ami-091138d0f0d41ff90"
+
+  }
+     variable "instance_type" {
+        default = "t3.micro"
+    }
+variable "key_name" {
+  default = "my-new-key"
+}
+variable "mysg" {
+ default = ["sg-0ae6f7e3e8be7e9d6"]
+  
+}
+variable "instance_name" {
+    default = "myec2"
+  
+}
